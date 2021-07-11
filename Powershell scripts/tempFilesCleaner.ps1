2db7ca41c8899df04d19d62efcbae26c383e4264
Damokless <damokless@protonmail.com>
2021-07-11 17:34:51 +0200

    fix tempFilesCleaner.ps1 and rename temp.cmd to tempFilesCleaner.cmd

--- Powershell scripts/tempFilesCleaner.ps1
+++ Powershell scripts/tempFilesCleaner.ps1
@@ -1,6 +1,6 @@
 $directoryPath = "$Env:WinDir\temp", "$Env:WinDir\prefetch", "$ENV:userprofile\AppData\Local\Temp"
 
-for ($i = 0; $i -lt $array.Count; $i++) {
+for ($i = 0; $i -lt $directoryPath.Count; $i++) {
     Set-Location $directoryPath[$i]
-    Remove-Item * -Recurse -Force -Confirm
+    Remove-Item * -Recurse -Force
 }
