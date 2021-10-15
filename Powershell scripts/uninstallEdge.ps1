0c1f38d5b985ff98bac91f97128db5ec07f74420
Damokless <damokless@protonmail.com>
2021-10-15 18:30:04 +0200

    adding comment

--- Powershell scripts/uninstallEdge.ps1
+++ Powershell scripts/uninstallEdge.ps1
@@ -1,4 +1,5 @@
 $pathName = Get-ChildItem ${env:PROGRAMFILES(X86)}\Microsoft\Edge\Application\ | Select-Object Name -First 1
 $versionFolder = $pathName.Name
 Set-Location ${env:PROGRAMFILES(X86)}\Microsoft\Edge\Application\${versionFolder}\Installer\
+#if script doesn't work copy this line and run it in cmd as administrator in c:\program files(x86)\Microsoft\Edge\Application\<version of edge>\Installer
 cmd.exe /c setup --uninstall --force-uninstall --system-level
