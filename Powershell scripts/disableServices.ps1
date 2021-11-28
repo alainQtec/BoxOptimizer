0feb313c9f25a95c609385ec196dc69904670b16
Alexandre Bouzon Conde <66098929+Damokless@users.noreply.github.com>
2021-11-28 16:55:31 +0100

    Revert "Add administrator requirements"

--- Powershell scripts/disableServices.ps1
+++ Powershell scripts/disableServices.ps1
@@ -1,5 +1,3 @@
-#Requires -RunAsAdministrator
-
 $servicesDisable = @(
     "AJRouter",
     "ALG",
--- Powershell scripts/tempFilesCleaner.ps1
+++ Powershell scripts/tempFilesCleaner.ps1
@@ -1,5 +1,3 @@
-#Requires -RunAsAdministrator
-
 $directoryPath = "$Env:WinDir\temp", "$Env:WinDir\prefetch", "$ENV:userprofile\AppData\Local\Temp"
 
 for ($i = 0; $i -lt $directoryPath.Count; $i++) {
