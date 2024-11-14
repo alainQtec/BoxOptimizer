c3aed2d0b2c6a91d17652e869480fe1a8414a5cc
Alain Herve <alainQtec@gmail.com>
2024-11-14 15:05:51 +0200

    Update BoxOptimizer.Integration.Tests.ps1

--- Tests/BoxOptimizer.Integration.Tests.ps1
+++ Tests/BoxOptimizer.Integration.Tests.ps1
@@ -1,13 +1,12 @@
 
 # verify the interactions and behavior of the module's components when they are integrated together.
 Describe "Integration tests: PsCraft" {
-    Context "Functionality Integration" {
-        It "Performs expected action" {
-            # Here you can write tests to simulate the usage of your functions and validate their behavior.
-            # For instance, if your module provides cmdlets to customize the command-line environment,
-            # you could simulate the invocation of those cmdlets and check if the environment is modified as expected.
-        }
+  Context "Functionality Integration" {
+    It "Performs expected action" {
+      # Here you can write tests to simulate the usage of your functions and validate their behavior.
+      # For instance, if your module provides cmdlets to customize the command-line environment,
+      # you could simulate the invocation of those cmdlets and check if the environment is modified as expected.
     }
-    # TODO: Add more contexts and tests as needed to cover various integration scenarios.
+  }
+  # TODO: Add more contexts and tests as needed to cover various integration scenarios.
 }
-      
