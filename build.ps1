3ba66ba8e7af3a0b874b9499f7c5b54b7831e3c7
Alain Herve <alainQtec@gmail.com>
2024-11-14 15:05:22 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -11,27 +11,25 @@
 #   License  : MIT
 [cmdletbinding(DefaultParameterSetName = 'task')]
 param(
-    [parameter(Position = 0, ParameterSetName = 'task')]
-    [ValidateScript({
-            $task_seq = [string[]]$_; $IsValid = $true
-            $Tasks = @('Init', 'Clean', 'Compile', 'Import', 'Test', 'Deploy')
-            ForEach ($name in $task_seq) {
-                $IsValid = $IsValid -and ($name -in $Tasks)
-            }
-            if ($IsValid) {
-                return $true
-            }
-            else {
-                throw [System.ArgumentException]::new('Task', "ValidSet: $($Tasks -join ', ').")
-            }
-        }
-    )][ValidateNotNullOrEmpty()]
-    [string[]]$Task = @('Init', 'Clean', 'Compile', 'Import'),
+  [parameter(Position = 0, ParameterSetName = 'task')]
+  [ValidateScript({
+      $task_seq = [string[]]$_; $IsValid = $true
+      $Tasks = @('Init', 'Clean', 'Compile', 'Import', 'Test', 'Deploy')
+      ForEach ($name in $task_seq) {
+        $IsValid = $IsValid -and ($name -in $Tasks)
+      }
+      if ($IsValid) {
+        return $true
+      } else {
+        throw [System.ArgumentException]::new('Task', "ValidSet: $($Tasks -join ', ').")
+      }
+    }
+  )][ValidateNotNullOrEmpty()]
+  [string[]]$Task = @('Init', 'Clean', 'Compile', 'Import'),
 
-    [parameter(ParameterSetName = 'help')]
-    [Alias('-Help')]
-    [switch]$Help
+  [parameter(ParameterSetName = 'help')]
+  [Alias('-Help')]
+  [switch]$Help
 )
 # Import the "buider module" and use Build-Module cmdlet to build this module:
 Import-Module PsCraft; Build-Module -Task $Task
-      
