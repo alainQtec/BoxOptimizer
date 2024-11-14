3d9b43d2f0745c285776658c7d1c062763c96a2f
Alain Herve <alainQtec@gmail.com>
2024-11-14 15:05:34 +0200

    Update BoxOptimizer.Features.Tests.ps1

--- Tests/BoxOptimizer.Features.Tests.ps1
+++ Tests/BoxOptimizer.Features.Tests.ps1
@@ -1,17 +1,16 @@
 
 Describe "Feature tests: PsCraft" {
-    Context "Feature 1" {
-        It "Does something expected" {
-            # Write tests to verify the behavior of a specific feature.
-            # For instance, if you have a feature to change the console background color,
-            # you could simulate the invocation of the related function and check if the color changes as expected.
-        }
+  Context "Feature 1" {
+    It "Does something expected" {
+      # Write tests to verify the behavior of a specific feature.
+      # For instance, if you have a feature to change the console background color,
+      # you could simulate the invocation of the related function and check if the color changes as expected.
     }
-    Context "Feature 2" {
-        It "Performs another expected action" {
-            # Write tests for another feature.
-        }
+  }
+  Context "Feature 2" {
+    It "Performs another expected action" {
+      # Write tests for another feature.
     }
-    # TODO: Add more contexts and tests to cover various features and functionalities.
+  }
+  # TODO: Add more contexts and tests to cover various features and functionalities.
 }
-      
