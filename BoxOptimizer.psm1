efdd4c60a3f2c9b3925da3bf8ec7ac1bcd556528
Alain Herve <alainQtec@gmail.com>
2024-11-14 15:05:14 +0200

    Update BoxOptimizer.psm1

--- BoxOptimizer.psm1
+++ BoxOptimizer.psm1
@@ -7,31 +7,28 @@ $Public = Get-ChildItem ([IO.Path]::Combine($PSScriptRoot, 'Public')) -Filter "*
 # Load dependencies
 $PrivateModules = [string[]](Get-ChildItem ([IO.Path]::Combine($PSScriptRoot, 'Private')) -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | Select-Object -ExpandProperty FullName)
 if ($PrivateModules.Count -gt 0) {
-    ForEach ($Module in $PrivateModules) {
-        Try {
-            Import-Module $Module -ErrorAction Stop
-        }
-        Catch {
-            Write-Error "Failed to import module $Module : $_"
-        }
+  ForEach ($Module in $PrivateModules) {
+    Try {
+      Import-Module $Module -ErrorAction Stop
+    } Catch {
+      Write-Error "Failed to import module $Module : $_"
     }
+  }
 }
 # Dot source the files
 ForEach ($Import in ($Public, $Private)) {
-    Try {
-        . $Import.fullname
-    }
-    Catch {
-        Write-Warning "Failed to import function $($Import.BaseName): $_"
-        $host.UI.WriteErrorLine($_)
-    }
+  Try {
+    . $Import.fullname
+  } Catch {
+    Write-Warning "Failed to import function $($Import.BaseName): $_"
+    $host.UI.WriteErrorLine($_)
+  }
 }
 # Export Public Functions
 $Param = @{
-    Function = $Public.BaseName
-    Variable = '*'
-    Cmdlet   = '*'
-    Alias    = '*'
+  Function = $Public.BaseName
+  Variable = '*'
+  Cmdlet   = '*'
+  Alias    = '*'
 }
 Export-ModuleMember @Param -Verbose
-      
